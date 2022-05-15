274304f15987dc27efe59877cde188a0bdbdefb7
Satendra Kumar <satendramca2016@gmail.com>
2022-05-15 09:16:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
+// import React, {useState, useRef, useEffect} from 'react';
 import "./App.scss";
+import LoginPage from "./LoginPage";
+import NextPage from "./NextPage";
 
 function App() {
   return (
-    <div className="container-grid">
-      <div className="container-grid-item">Graph</div>
-
-      <div className="container-grid-item container">
-        <form >
-          <h1> Create an account</h1>
-          <label className="container__label">Your Email Address</label> <br />
-          <input type="text" />
-          <br />
-          <label className="container__label">Your Email Address</label> <br />
-          <input type="password" />
-          <br />
-          <label className="container__label">Your Email Address</label> <br />
-          <input type="password" />
-          <br />
-          <label className="container__label">Your Email Address</label> <br />
-          <input type="text" />
-          <br />
-          <label className="container__label">Your Email Address</label> <br />
-          <input type="number" />
-          <br /> 
-          <input  className="checkbox__align" type="checkbox"/>
-          <label className="container__label">Term and conditions</label><br/>
-          <button type="submit" className="container__btn">
-            {" "}
-            Create account
-          </button>
-        </form>
-      </div>
+    <div>
+      {/* <LoginPage/> */}
+      <NextPage/>
     </div>
   );
 }
---
+++ src/LoginPage.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className="container-grid">
+    <div className="container-grid-item">Graph</div>
+
+    <div className="container-grid-item container">
+      <form >
+        <h1> Create an account</h1>
+        <label className="container__label">Your Email Address</label> <br />
+        <input type="text" />
+        <br />
+        <label className="container__label">Your Email Address</label> <br />
+        <input type="password" />
+        <br />
+        <label className="container__label">Your Email Address</label> <br />
+        <input type="password" />
+        <br />
+        <label className="container__label">Your Email Address</label> <br />
+        <input type="text" />
+        <br />
+        <label className="container__label">Your Email Address</label> <br />
+        <input type="number" />
+        <br /> 
+        <input  className="checkbox__align" type="checkbox"/>
+        <label className="container__label">Term and conditions</label><br/>
+        <button type="submit" className="container__btn">
+          {" "}
+          Create account
+        </button>
+      </form>
+    </div>
+  </div>
+  )
+}
+
+export default LoginPage
