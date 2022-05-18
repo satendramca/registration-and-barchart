1bf6e03d22c6c02926a29e12e696dfdf267df3c4
Satendra Kumar <satendramca2016@gmail.com>
2022-05-18 09:34:35 +0530

    remove code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import React, {useState, useRef, useEffect} from 'react';
 import "./App.scss";
 import FormValidation from "./FormValidation";
-// import LoginPage from "./LoginPage";
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-// import NextPage from './NextPage';
-// import NextPage from "./NextPage1";
-// import Success from './Success';
-import Chart from './Component/Chart';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chart from "./Component/Chart";
 
 function App() {
-  
-
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-         <Route path='/' element={<FormValidation/>}></Route> 
-        <Route path='/success' element={<Chart/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<FormValidation />}></Route>
+          <Route path="/success" element={<Chart />}></Route>
+        </Routes>
       </BrowserRouter>
-      {/* <LoginPage/> */}
-      {/* <FormValidation/>
-      <Chart/> */}
-      {/* <Success/> */}
-      {/* <NextPage1/> */}
     </div>
   );
 }
 
-
-
 export default App;
-
-
-
-
-
-
